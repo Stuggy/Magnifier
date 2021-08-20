72281daff81d4162ce010ffb4191185120888bff
Stuggy <17179442+Stuggy@users.noreply.github.com>
2021-08-20 15:58:46 +0200

    Another working version - different code while I explore and try and add things but visually the same as the first version...

--- lib/main.dart
+++ lib/main.dart
@@ -2,48 +2,71 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
-List<CameraDescription> cameras = [] ;
+List<CameraDescription> cameras = [] ;  // I added the default initialisation
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+	WidgetsFlutterBinding.ensureInitialized();
 
-  cameras = await availableCameras();
-  runApp(CameraApp());
+	cameras = await availableCameras();
+	runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {   // This is where the app starts
+	// This widget is the root of your application.
+	@override
+	Widget build(BuildContext context) {
+
+
+
+	 	return MaterialApp(
+			/* appBar: AppBar(
+			title: Text('test'),
+			), */
+			title: 'Flutter Demo',
+			theme: ThemeData(
+				primarySwatch: Colors.blue,
+			),
+			home: CameraApp(),
+		); 
+	}
 }
 
 class CameraApp extends StatefulWidget {
-  @override
-  _CameraAppState createState() => _CameraAppState();
+	@override
+	_CameraAppState createState() => _CameraAppState();
 }
 
+/// This is the private State class that goes with CameraApp StatefulWidget.
 class _CameraAppState extends State<CameraApp> {
-  late CameraController controller;  // I added late stating that it would be initialised later
+	late CameraController controller;  // I added late stating that it would be initialised later
 
-  @override
-  void initState() {
-    super.initState();
-    controller = CameraController(cameras[0], ResolutionPreset.max);
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    });
-  }
+	@override
+	void initState() {
+		super.initState();
+		controller = CameraController(cameras[0], ResolutionPreset.max);
+		controller.initialize().then((_) {
+			if (!mounted) {
+				return;
+			}
+			setState(() {});
+		});
+	}
 
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+	@override
+	void dispose() {
+		controller.dispose();
+		super.dispose();
+	}
 
-  @override
-  Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return Container();
-    }
-    return MaterialApp(
-      home: CameraPreview(controller),
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		if (!controller.value.isInitialized) {
+			return Container();
+		}
+		return AspectRatio(		// Using Aspectratio got the camera to show again
+			aspectRatio: controller.value.aspectRatio,	// just sets the aspectratio of the view I think
+        	child: CameraPreview(controller)
+		);
+
+	}
 }
