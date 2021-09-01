6a4ea5d74a141f28280c112fdcc6e5e70c74ad31
Stuggy <17179442+Stuggy@users.noreply.github.com>
2021-09-01 15:24:38 +0200

    Camera selection is working (using a row of available cameras) Pinch and zoom is working on the preview

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,39 @@ import 'package:camera/camera.dart';
 List<CameraDescription> cameras = []; // I added the default initialisation
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  cameras = await availableCameras();
+  // Fetch the available cameras before initializing the app.
+	try {
+		WidgetsFlutterBinding.ensureInitialized();
+		cameras = await availableCameras();
+	} on CameraException catch (e) {
+    	logError(e.code, e.description);
+  }
   runApp(MyApp());
 }
+
+void logError(String code, String? message) {
+  if (message != null) {
+    print('Error: $code\nError Message: $message');
+  } else {
+    print('Error: $code');
+  }
+}
+
+/// Returns a suitable camera icon for [direction].
+IconData getCameraLensIcon(CameraLensDirection direction) {
+  switch (direction) {
+    case CameraLensDirection.back:
+      return Icons.camera_rear;
+    case CameraLensDirection.front:
+      return Icons.camera_front;
+    case CameraLensDirection.external:
+      return Icons.camera;
+    default:
+      throw ArgumentError('Unknown lens direction');
+  }
+}
+
+ 
 
 class MyApp extends StatelessWidget {
   // This is where the app starts
@@ -33,10 +61,19 @@ class CameraApp extends StatefulWidget {
 }
 
 /// This is the private State class that goes with CameraApp StatefulWidget above
-class _CameraAppState extends State<CameraApp> {
-  XFile? imageFile; // the file for the camera capture
-  late CameraController
-      controller; // I added late stating that it would be initialised later
+class _CameraAppState extends State<CameraApp> with WidgetsBindingObserver {
+//  XFile? imageFile; // the file for the camera capture
+  late CameraController controller; // I added late stating that it would be initialised later
+  double _minAvailableExposureOffset = 0.0;		// I get info field not used because it is not read
+  double _maxAvailableExposureOffset = 0.0;
+  double _currentExposureOffset = 0.0;
+  double _minAvailableZoom = 1.0;
+  double _maxAvailableZoom = 1.0;
+  double _currentScale = 1.0;
+  double _baseScale = 1.0;
+
+   // Counting pointers (number of user fingers on screen)
+  int _pointers = 0;
 
   @override
   void initState() {
@@ -52,8 +89,24 @@ class _CameraAppState extends State<CameraApp> {
 
   @override
   void dispose() {
-    controller.dispose();
+ //   controller.dispose();
     super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final CameraController? cameraController = controller;
+
+    // App state changed before we got the chance to initialize.
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      onNewCameraSelected(cameraController.description);
+    }
   }
 
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -66,14 +119,17 @@ class _CameraAppState extends State<CameraApp> {
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
-        title: const Text('Home Route'),
+        title: const Text('Appbar title'),
       ),
       body: Stack(
         children: <Widget>[
           Center(
-            child: CameraPreview(controller),
+      //      child: CameraPreview(controller),
+			child: _cameraPreviewWidget(),
           ),
+		  _cameraTogglesRowWidget()
         ],
+		
       ), // end of Stack
       floatingActionButton: FloatingActionButton(
         elevation: 10.0,
@@ -111,9 +167,162 @@ class _CameraAppState extends State<CameraApp> {
         //}
       ),
     );  // end of scaffold
+	
   } // BuildContext for camerapp
+
+  /// Display the preview from the camera (or a message if the preview is not available).
+  Widget _cameraPreviewWidget() {
+    final CameraController? cameraController = controller;
+
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return const Text(
+        'Tap a camera',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    } else {
+      return Listener(
+        onPointerDown: (_) => _pointers++,
+        onPointerUp: (_) => _pointers--,
+        child: CameraPreview(
+          controller,
+          child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+            return GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onScaleStart: _handleScaleStart,
+              onScaleUpdate: _handleScaleUpdate,
+              onTapDown: (details) => onViewFinderTap(details, constraints),
+            );
+          }),
+        ),
+      );
+    }
+  }
+
+  void onViewFinderTap(TapDownDetails details, BoxConstraints constraints) {
+    if (controller == null) {
+      return;
+    }
+
+    final CameraController cameraController = controller;
+
+    final offset = Offset(
+      details.localPosition.dx / constraints.maxWidth,
+      details.localPosition.dy / constraints.maxHeight,
+    );
+    cameraController.setExposurePoint(offset);
+    cameraController.setFocusPoint(offset);
+  }
+
+  void _handleScaleStart(ScaleStartDetails details) {
+    _baseScale = _currentScale;
+  }
+
+  Future<void> _handleScaleUpdate(ScaleUpdateDetails details) async {
+    // When there are not exactly two fingers on screen don't scale
+    if (controller == null || _pointers != 2) {
+      return;
+    }
+
+    _currentScale = (_baseScale * details.scale)
+        .clamp(_minAvailableZoom, _maxAvailableZoom);
+
+    await controller.setZoomLevel(_currentScale);
+  }
+
+
+/// Display a row of toggle to select the camera (or a message if no camera is available).
+  Widget _cameraTogglesRowWidget() {
+    final List<Widget> toggles = <Widget>[];
+
+    final onChanged = (CameraDescription? description) {
+      if (description == null) {
+        return;
+      }
+
+      onNewCameraSelected(description);
+    };
+
+    if (cameras.isEmpty) {
+      return const Text('No camera found');
+    } else {
+      for (CameraDescription cameraDescription in cameras) {
+        toggles.add(
+          SizedBox(
+            width: 90.0,
+            child: RadioListTile<CameraDescription>(
+              title: Icon(getCameraLensIcon(cameraDescription.lensDirection)),
+              groupValue: controller.description,
+              value: cameraDescription,
+              onChanged:
+                  controller != null && controller.value.isRecordingVideo
+                      ? null
+                      : onChanged,
+            ),
+          ),
+        );
+      }
+    }
+
+    return Row(children: toggles);
+  }
   
-  void showInSnackBar(String message) {
+	void onNewCameraSelected(CameraDescription cameraDescription) async {
+   //   await controller.dispose();	// You can't use the controller after it is disposed of.  This gets rid of the used after disposed error.
+
+    final CameraController cameraController = CameraController(
+      cameraDescription,
+      ResolutionPreset.medium,
+   //   enableAudio: enableAudio,
+      imageFormatGroup: ImageFormatGroup.jpeg,
+    );
+
+    controller = cameraController;
+
+    // If the controller is updated then update the UI.
+    cameraController.addListener(() {
+      if (mounted) setState(() {});
+      if (cameraController.value.hasError) {
+        showInSnackBar(
+            'Camera error ${cameraController.value.errorDescription}');
+      }
+    });
+
+    try {
+      await cameraController.initialize();
+      await Future.wait([
+        cameraController
+            .getMinExposureOffset()
+            .then((value) => _minAvailableExposureOffset = value),
+        cameraController
+            .getMaxExposureOffset()
+            .then((value) => _maxAvailableExposureOffset = value),
+        cameraController
+            .getMaxZoomLevel()
+            .then((value) => _maxAvailableZoom = value),
+        cameraController
+            .getMinZoomLevel()
+            .then((value) => _minAvailableZoom = value),
+      ]);
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }	// end of onNewCameraSelected
+
+	void _showCameraException(CameraException e) {
+    logError(e.code, e.description);
+    showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+  
+	void showInSnackBar(String message) {
       // I had to move this class within the _CameraAppState or _scaffoldKey was undefined
    //   _scaffoldKey.currentState?.showSnackBar(SnackBar(content: Text(message)));
       ScaffoldMessenger.of(context).showSnackBar(
